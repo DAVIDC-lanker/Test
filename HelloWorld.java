70d9adc743cefac2561038c86ce911743193acaf
David-C <david.cao3@student.tdsb.on.ca>
2025-09-11 22:12:33 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+    public class HelloWorld {
+
+        public static void main(String[] args) {
+            Scanner keyboard = new Scanner(System.in);
+            
+            System.out.println("give a side length: ");
+            double sideB = keyboard.nextDouble();
+            
+            System.out.println("give another side length: ");
+            double sideC = keyboard.nextDouble(); 
+
+            System.out.println("give an angle: ");
+            double angleA = keyboard.nextDouble(); 
+
+            double B = Math.asin((sideB * angleA)/sideC);
+            
+            double angleB = 180 - B;
+
+            double C = 180 - angleA - angleB;
+            
+            double sideA = (Math.sin(C)/Math.sin(angleA))*sideC;
+
+            System.out.println("here is your side length: "+sideA);
+            keyboard.close();
+            ;
+
+                
+        }
+    }
+
